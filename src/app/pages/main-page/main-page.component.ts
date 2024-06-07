902caafbb19564370b867c150ead457eed5dd541
sergio-grr <sejagarrido@gmail.com>
2024-06-07 10:09:11 +0200

    animation

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -6,17 +6,24 @@ import { AfterViewInit, Component, ElementRef, OnInit, Renderer2 } from '@angula
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent  implements AfterViewInit {
+
   constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
   ngAfterViewInit() {
     const animatedContainer = this.elementRef.nativeElement.querySelector('#animatedcontainer');
+    const animation = this.elementRef.nativeElement.querySelector('#animation');
 
     animatedContainer.addEventListener('animationend', () => {
       this.renderer.setStyle(animatedContainer, 'display', 'none');
     });
 
+    animation.addEventListener('animationend', () => {
+      this.renderer.setStyle(animation, 'display', 'none');
+    });
+
     setTimeout(() => {
       this.renderer.addClass(animatedContainer, 'fade-out');
-    }, 2000);
+      this.renderer.addClass(animation, 'fade-out');
+    }, 500);
   }
 }
