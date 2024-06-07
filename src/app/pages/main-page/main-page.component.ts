0cf65f7cba70e4f00165837bdddd2e7f416945dc
sergio-grr <sejagarrido@gmail.com>
2024-06-07 09:28:50 +0200

    animacioninicial

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent {
+export class MainPageComponent  implements AfterViewInit {
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
+  ngAfterViewInit() {
+    const animatedContainer = this.elementRef.nativeElement.querySelector('#animatedcontainer');
 
+    animatedContainer.addEventListener('animationend', () => {
+      this.renderer.setStyle(animatedContainer, 'display', 'none');
+    });
+
+    setTimeout(() => {
+      this.renderer.addClass(animatedContainer, 'fade-out');
+    }, 2000);
+  }
 }
